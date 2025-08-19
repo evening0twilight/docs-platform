c8fc3ca41fa3b43d8f4869bf9e549173bc4782f8
evening0twilight <980898953@qq.com>
2025-08-19 15:29:38 +0800

    feat:计划加入路由，创建登录，并且设定登录背景是蜘蛛网效果

---
+++ src/views/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+/**
+* @description 
+*/
+import { ref, onMounted, reactive, toRefs } from 'vue';
+
+// interface State {
+//   : any
+// }
+
+// const state = reactive<State>({
+//   : null
+// });
+
+// const {
+  
+// } = toRefs(state);
+
+</script>
+<script src="@/styles/zhizhu.js"></script>
+
+<style scoped>
+
+</style>
