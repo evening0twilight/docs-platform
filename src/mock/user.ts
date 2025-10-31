1f7b50e1ed01d9f892de974ee36209264b6b5dfd
evening0twilight <980898953@qq.com>
2025-10-31 19:20:17 +0800

    fix:修复mock数据没有更新的问题

--- src/mock/user.ts
+++ src/mock/user.ts
@@ -3,6 +3,7 @@ import type { UserInfo } from '@/components/type';
 // 初始模拟数据
 export const mockUserData: UserInfo = {
   id: '10001',
+  username: '张三',
   name: '张三',
   email: 'zhangsan@example.com',
   avatar: 'https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/3ee5f13fb09879ecb5185e440cef6eb9.png~tplv-uwbnlip3yd-webp.webp',
